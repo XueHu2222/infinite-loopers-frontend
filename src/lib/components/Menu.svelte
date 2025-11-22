d29bb891a3916779d95ba5f2a6ee99afb3bff4e0
Xue Hu <hu0018@hz.nl>
2025-11-22 14:11:30 +0100

    create menu component

---
+++ src/lib/components/Menu.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/assets/logo.png';
+	export let items = [];
+
+	$: currentPath = $page.url.pathname;
+</script>
+
+<header class="flex w-full items-center justify-between bg-[#EEE9E1] px-6 py-5">
+	<div class="flex items-center gap-3">
+		<img src={logo} alt="Logo" class="h-8 w-8" />
+		<div class="text-xl font-bold text-[#4F3117]">Quest Giver</div>
+	</div>
+
+	<div class="flex items-center gap-6">
+		<nav class="flex items-center gap-6">
+			{#each items as item}
+				<a
+					href={item.href}
+					class="px-1 text-lg
+          {currentPath === item.href
+						? 'font-semibold text-[#3E2612]'
+						: 'text-[#4F3117] hover:text-[#7A5C3E]'}"
+				>
+					{item.label}
+				</a>
+			{/each}
+		</nav>
+		<button
+			class="ml-4 rounded bg-[#4F3117] px-4 py-1 font-medium text-[#EEE9E1] hover:bg-[#3E2612]"
+		>
+			Login
+		</button>
+	</div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,15 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
+	import Menu from '$lib/components/Menu.svelte';
+
 	let { children } = $props();
 </script>
+
+<Menu items={[
+	{ label: 'Home', href: '/' },
+	{ label: 'Task', href: '/task' }
+]} />
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
