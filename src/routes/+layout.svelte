cead3ac5700cd0af40f8a7afaa43279d90a19023
Dimitar871 <dayossifov@gmail.com>
2025-12-09 16:35:24 +0100

    Achievements frondend page with dummy data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,8 @@
 
 <Menu items={[
 	{ label: 'Home', href: '/home' },
-	{ label: 'Quest Log', href: '/quest-log' }
+	{ label: 'Quest Log', href: '/quest-log' },
+	{ label: 'Achievements', href: '/achievements' },
 ]} />
 
 <svelte:head>
